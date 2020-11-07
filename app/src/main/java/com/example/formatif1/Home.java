7daadb025dbd14f5878789342e06b2c751f135de
chanaseptiari <chanaseptiari@gmail.com>
2020-11-07 19:13:54 +0700

    Initial commit

--- app/src/main/java/com/example/formatif1/Home.java
+++ app/src/main/java/com/example/formatif1/Home.java
@@ -1,13 +1,48 @@
 package com.example.formatif1;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Home extends AppCompatActivity {
-
+public class Home extends AppCompatActivity implements View.OnClickListener{
+Button btnLogout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        btnLogout = findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnLogout){
+            AlertDialog.Builder Notif = new AlertDialog.Builder(this);
+            Notif.setTitle("Logout");
+            Notif.setMessage("Ya untuk Logout")
+                    .setCancelable(true)
+                    .setNegativeButton("Batal", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent logout = new Intent();
+                            logout.putExtra("Logout","Anda Telah Logout");
+                            setResult(Activity.RESULT_OK,logout);
+                            finish();
+                        }
+                    });
+            Notif.show();
+        }
     }
 }
--- app/src/main/java/com/example/formatif1/Register.java
+++ app/src/main/java/com/example/formatif1/Register.java
@@ -2,13 +2,29 @@ package com.example.formatif1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Register extends AppCompatActivity {
+public class Register extends AppCompatActivity implements View.OnClickListener {
+Button btnDaftar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        btnDaftar = findViewById(R.id.btnDaftar);
+
+        btnDaftar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.btnDaftar){
+            Intent login = new Intent(this,MainActivity.class);
+            startActivity(login);
+        }
+
     }
 }
